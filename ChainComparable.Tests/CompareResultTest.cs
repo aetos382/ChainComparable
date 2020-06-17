8d93712962524d47bab1af90bac953feb8d7c34c
aetos <aetos@aerie.jp>
2020-06-17 19:58:35 +0900

    テスト

--- ChainComparable.Tests/CompareResultTest.cs
+++ ChainComparable.Tests/CompareResultTest.cs
@@ -13,5 +13,33 @@ namespace ChainComparable.Tests
             Assert.True(trueResult);
             Assert.False(falseResult);
         }
+
+        [Fact]
+        public void Tにキャストしてみる()
+        {
+            var result = new CompareResult<int>(default, 100);
+
+            Assert.Equal(100, result);
+        }
+
+        [Fact]
+        public void Negateのテスト()
+        {
+            var trueResult = new CompareResult<int>(true, default);
+            var falseResult = new CompareResult<int>(false, default);
+
+            Assert.False(trueResult.Negate());
+            Assert.True(falseResult.Negate());
+        }
+
+        [Fact]
+        public void Equalsのテスト()
+        {
+            var result = new CompareResult<int>(default, 100);
+
+            Assert.False(result.Equals(99));
+            Assert.True(result.Equals(100));
+            Assert.False(result.Equals(101));
+       }
     }
 }
