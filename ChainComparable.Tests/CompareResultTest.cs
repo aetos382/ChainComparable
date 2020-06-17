1dded8bec842e8ebc29292b5aeb43d0fdc213ffa
aetos <aetos@aerie.jp>
2020-06-17 20:31:57 +0900

    テストを追加

--- ChainComparable.Tests/CompareResultTest.cs
+++ ChainComparable.Tests/CompareResultTest.cs
@@ -40,6 +40,52 @@ namespace ChainComparable.Tests
             Assert.False(result.Equals(99));
             Assert.True(result.Equals(100));
             Assert.False(result.Equals(101));
-       }
+        }
+
+        [Fact]
+        public void Equalsでnullとnull以外を比較すると等しくない()
+        {
+            var nullResult = new CompareResult<string>(default, null);
+            var nonNullResult = new CompareResult<string>(default, string.Empty);
+
+            Assert.False(nonNullResult.Equals(null));
+            Assert.False(nullResult.Equals(string.Empty));
+        }
+        
+        [Fact]
+        public void Equalsでnull同士を比較すると等しい()
+        {
+            var result = new CompareResult<string>(default, null);
+
+            Assert.True(result.Equals(null));
+        }
+
+        [Fact]
+        public void CompareToのテスト()
+        {
+            var result = new CompareResult<int>(default, 100);
+
+            Assert.True(result.CompareTo(99) > 0);
+            Assert.True(result.CompareTo(100) == 0);
+            Assert.True(result.CompareTo(99) > 0);
+        }
+
+        [Fact]
+        public void CompareToでnullとnull以外の値を比較するとnullは最小値とみなされる()
+        {
+            var nullResult = new CompareResult<string>(default, null);
+            var nonNullResult = new CompareResult<string>(default, string.Empty);
+
+            Assert.True(nonNullResult.CompareTo(null) > 0);
+            Assert.True(nullResult.CompareTo(string.Empty) < 0);
+        }
+
+        [Fact]
+        public void CompareToでnull同士を比較すると等しい()
+        {
+            var result = new CompareResult<string>(default, null);
+
+            Assert.Equal(0, result.CompareTo(null));
+        }
     }
 }
