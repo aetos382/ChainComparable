682c898f5ac879f35a546477088427fffd3c7e6a
aetos <aetos@aerie.jp>
2020-06-17 19:43:11 +0900

    add CompareResultTest

---
+++ ChainComparable.Tests/CompareResultTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace ChainComparable.Tests
+{
+    public class CompareResultTest
+    {
+        [Fact]
+        public void boolにキャストしてみる()
+        {
+            var trueResult = new CompareResult<int>(true, default);
+            var falseResult = new CompareResult<int>(false, default);
+
+            Assert.True(trueResult);
+            Assert.False(falseResult);
+        }
+    }
+}
