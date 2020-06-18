df8e4e7f686b2a78973d63d286f70ad3f030656a
aetos <aetos@aerie.jp>
2020-06-19 03:30:24 +0900

    add tests

---
+++ ChainComparable.Tests/ChainComparerTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using Xunit;
+
+namespace ChainComparable.Tests
+{
+    public class ChainComparerTest
+    {
+        [Fact]
+        public void DefaultEqualityComparerはnullじゃない()
+        {
+            Assert.NotNull(ChainComparer<int>.DefaultEqualityComparer);
+        }
+
+        [Fact]
+        public void DefaultComparerはnullじゃない()
+        {
+            Assert.NotNull(ChainComparer<int>.DefaultComparer);
+        }
+
+        [Fact]
+        public void EqualityComparerの既定値はnullじゃない()
+        {
+            Assert.NotNull(ChainComparer<int>.EqualityComparer);
+        }
+
+        [Fact]
+        public void Comparerの既定値はnullじゃない()
+        {
+            Assert.NotNull(ChainComparer<int>.Comparer);
+        }
+
+        [Fact]
+        public void EqualityComparerにnullをセットするとArgumentNullExceptionが飛ぶ()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => {
+                    ChainComparer<int>.EqualityComparer = null;
+                });
+        }
+
+        [Fact]
+        public void ComparerにnullをセットするとArgumentNullExceptionが飛ぶ()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => {
+                    ChainComparer<int>.Comparer = null;
+                });
+        }
+
+        [Fact]
+        public void InternalGetHashCodeはnullを渡すと0を返す()
+        {
+            Assert.Equal(0, ChainComparer<string>.InternalGetHashCode(null));
+        }
+    }
+}
